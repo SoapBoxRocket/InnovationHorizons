742a7b48c8a4f751e16bdcf01f3554d53d2e4c28
SoapBoxRocket <Craig@Soap-Box-Rocket.local>
2017-03-13 12:54:47 -0700

    PHP form
    improved PHP form to self echo

--- index.php
+++ index.php
@@ -47,18 +47,50 @@
 }
 </style>
 <body>
-    <center>
-      <img id="graph" src="images/HorizonsGraph.png">
-      <canvas id='graphCanvas' width='500px' height='500px' style="border:1px solid #000000"></canvas>
-      <form action="welcome.php" method="post">
-        Project Name: <input type="text" name="projName"><br>
-        Market<br>Exsisting <input type="range" name="market" min="0" max="10"> New to World<br>
-        <input type="submit">
-      </form>
-      <div>
-        <input id='draw-btn' type='button' value='DRAW' onclick='Draw()' />
-      </div>
-    </center>
+  <?php
+    $projName = $market = $solution = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $projName = test_input($_POST["projName"]);
+      $market = test_input($_POST["market"]);
+      $solution = test_input($_POST["solution"]);
+    }
+
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripcslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+  ?>
+  <center>
+    <img id="graph" src="images/HorizonsGraph.png">
+    <canvas id='graphCanvas' width='500px' height='500px' style="border:1px solid #000000"></canvas>
+    <form action="<?php echo htmlspecialchars ($_SERVER["PHP_SELF"]);?>" method="post">
+      Project Name: <input type="text" name="projName"><br>
+      Market<br>Exsisting <input type="range" name="market" min="0" max="10" value="0" step=".5"> New to World<br>
+      Solution<br>Exsisting <input type="range" name="solution" min="0" max="10" value="0" step=".5"> New to World<br>
+      <input type="submit">
+    </form>
+    <div>
+      <input id='draw-btn' type='button' value='DRAW' onclick='Draw()' />
+    </div>
+  </center>
+  <br>
+  <br>
+  <center>
+    <?php
+      echo "Project: ";
+      echo $projName;
+      echo "<br>";
+      echo "Market: ";
+      echo $market;
+      echo "<br>";
+      echo "Solution ";
+      echo $solution;
+      echo "<br>";
+    ?>
+  </center>
   <div id="container">
     <div id="circle">&#x25CB;</div>
     <div id="text">1</div>
