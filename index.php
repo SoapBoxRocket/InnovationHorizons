189d12800fad03a712e75fb2850d31902b8bcf38
SoapBoxRocket <Craig@Soap-Box-Rocket.local>
2017-03-13 13:47:48 -0700

    input verification
    all 3 inputs are verified for proper values

--- index.php
+++ index.php
@@ -24,6 +24,9 @@
       ctx.fillText ("1", 46, 55);
     }
   </script>
+  <style>
+    .error {color: #FF0000;}
+  </style>
 </head>
 <style>
 #container {
@@ -48,12 +51,27 @@
 </style>
 <body>
   <?php
-    $projName = $market = $solution = "";
+    $nameErr = $marketErr = $solutionErr = "";
+    $name = $market = $solution = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      $projName = test_input($_POST["projName"]);
-      $market = test_input($_POST["market"]);
-      $solution = test_input($_POST["solution"]);
+      if (empty($_POST["name"])) {
+        $nameErr = "Pleae provide a project name.";
+      } else {
+          $name = test_input($_POST["name"]);
+      }
+
+      if ($_POST["market"] == 0) {
+        $marketErr = "Please estimate the market fit for this project.";
+      } else {
+        $market = test_input($_POST["market"]);
+      }
+
+      if ($_POST["solution"] == 0) {
+        $solutionErr = "Please estimate the solution fit for this project.";
+      } else {
+        $solution = test_input($_POST["solution"]);
+      }
     }
 
     function test_input($data) {
@@ -67,9 +85,12 @@
     <img id="graph" src="images/HorizonsGraph.png">
     <canvas id='graphCanvas' width='500px' height='500px' style="border:1px solid #000000"></canvas>
     <form action="<?php echo htmlspecialchars ($_SERVER["PHP_SELF"]);?>" method="post">
-      Project Name: <input type="text" name="projName"><br>
-      Market<br>Exsisting <input type="range" name="market" min="0" max="10" value="0" step=".5"> New to World<br>
-      Solution<br>Exsisting <input type="range" name="solution" min="0" max="10" value="0" step=".5"> New to World<br>
+      Project Name: <input type="text" name="name">
+      <span class="error"> <?php echo $nameErr;?></span><br>
+      Market<br>Exsisting <input type="range" name="market" min="0" max="10" value="0" step=".5"> New to World
+      <span class="error"> <?php echo $marketErr;?></span><br>
+      Solution<br>Exsisting <input type="range" name="solution" min="0" max="10" value="0" step=".5"> New to World
+      <span class="error"> <?php echo $solutionErr;?></span><br>
       <input type="submit">
     </form>
     <div>
@@ -81,7 +102,7 @@
   <center>
     <?php
       echo "Project: ";
-      echo $projName;
+      echo $name;
       echo "<br>";
       echo "Market: ";
       echo $market;
